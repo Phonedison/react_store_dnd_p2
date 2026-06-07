2d81cf467a5f49d64b5cd6be0e3663bfbe7b3673
Lucas Leal da Silva <lucasleal00100@gmail.com>
2026-06-07 14:56:35 -0300

    feat: implementando método da imagem

--- src/features/Moster/CardMonster/index.jsx
+++ src/features/Moster/CardMonster/index.jsx
@@ -1,42 +1,42 @@
-import { useEffect, useState } from "react";
 import { DivComponents } from "../../../components/Div";
 import { ImageComponents } from "../../../components/Img";
+import { useElementListImage } from "../../../services/CustomHooks";
 
 const imagem_padrao =
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTErdgbxMUdTJs1QVlGKym_uTNRaF_OQEk3XA&s";
 
 export const CardEnemy = ({ enemy }) => {
-  const [detailEnemy, setDetailEnemy] = useState(null);
   // const navigate = useNavigate();
   // const navigateToDetails = () => {
   //   navigate(enemy.index);
   // };
 
-  useEffect(() => {
-    fetch(`https://www.dnd5eapi.co${enemy.url}`)
-      .then((res) => res.json())
-      .then((data) => setDetailEnemy(data))
-      .catch((error) => console.error("Erro ao carregar detalhes", error));
-  }, [enemy.url]);
+  const { object, erro, loading } = useElementListImage(enemy);
 
-  if (!detailEnemy) {
+  if (!loading) {
     return <DivComponents>Carregando dados de {enemy.$name}...</DivComponents>;
   }
 
-  const urlImages = detailEnemy?.image
-    ? `https://www.dnd5eapi.co${detailEnemy.image}`
+  if (erro) {
+    return (
+      <DivComponents>Erro ao carregar imagem de {enemy.name}</DivComponents>
+    );
+  }
+
+  const urlImages = object?.image
+    ? `https://www.dnd5eapi.co${object.image}`
     : imagem_padrao;
 
   return (
     <DivComponents $name="DoodleCardScene">
-      <DivComponents $name="DoodleCardInner" key={detailEnemy.index} $scale>
+      <DivComponents $name="DoodleCardInner" key={object.index} $scale>
         <DivComponents $name="DoodleCardFront">
           <DivComponents $name="DoodleTitle" className="doodle-title">
             {enemy.name}
           </DivComponents>
           <ImageComponents
             src={urlImages}
-            alt={`imagem representativa de ${detailEnemy.name}`}
+            alt={`imagem representativa de ${object.name}`}
           />
         </DivComponents>
       </DivComponents>
