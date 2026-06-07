69191649e37f75117d71279dbd543f4065440205
Lucas Leal da Silva <lucasleal00100@gmail.com>
2026-06-07 14:55:04 -0300

    feat: alteração na apresentação

--- src/services/dndAPI/index.jsx
+++ src/services/dndAPI/index.jsx
@@ -11,7 +11,7 @@ const apiDnD = axios.create({
  * Se o `id` for omitido (ou `null`), busca a lista completa do recurso. Caso contrário, busca o item específico.
  * O idioma padrão é o inglês ('en') caso nenhum seja especificado.
  *
- * @param {string|number|null} [id=null] - ID opcional do elemento (ex: 'fireball', 25).
+ * @param {'string'|'number'|'null'} [id=null,lang=null] - ID opcional do elemento (ex: 'fireball', 25).
  * @param {string} element - O endpoint/recurso desejado (ex: '/spells', '/monsters', 'etc').
  * @param {string|null} [lang='en'] - O código do idioma (ex: 'fr-FR','pt-Br'). Obs: O padrão é 'en'.
  * @returns {Promise<AxiosResponse>} Promise com a resposta da API.
