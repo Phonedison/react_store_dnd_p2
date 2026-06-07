9fe93870ca9aaa22ba4666c1a07fb37e4fd081e0
Lucas Leal da Silva <lucasleal00100@gmail.com>
2026-06-07 12:34:19 -0300

    feat: Melhorando o método de busca dos dados da API e add descrição de uso.

--- src/services/dndAPI/index.jsx
+++ src/services/dndAPI/index.jsx
@@ -4,8 +4,23 @@ const apiDnD = axios.create({
   baseURL: "https://www.dnd5eapi.co/api/2014",
 });
 
-export const getElements = (id = null, element) => {
+/**
+ * Busca de recursos na API dnd5eAPI, permitindo listagem geral ou dados por ID, com suporte ao Idioma.
+ * * @description
+ * O método monta a URL da requisição se baseando nos parâmetros fornecidos.
+ * Se o `id` for omitido (ou `null`), busca a lista completa do recurso. Caso contrário, busca o item específico.
+ * O idioma padrão é o inglês ('en') caso nenhum seja especificado.
+ *
+ * @param {string|number|null} [id=null] - ID opcional do elemento (ex: 'fireball', 25).
+ * @param {string} element - O endpoint/recurso desejado (ex: '/spells', '/monsters', 'etc').
+ * @param {string|null} [lang='en'] - O código do idioma (ex: 'fr-FR','pt-Br'). Obs: O padrão é 'en'.
+ * @returns {Promise<AxiosResponse>} Promise com a resposta da API.
+ */
+export const getElements = (element, id = null, lang = null) => {
+  const idioma = lang !== null ? lang : "en";
   const url =
-    id === null ? `${element}?lang=pt-BR` : `${element}/${id}?lang=pt-BR`;
+    id === null
+      ? `${element}?lang=${idioma}`
+      : `${element}/${id}?lang=${idioma}`;
   return apiDnD.get(url);
 };
