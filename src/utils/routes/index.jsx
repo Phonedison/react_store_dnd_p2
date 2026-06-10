1d25a76a1e6a7958802ceb49708c21bed827a79d
YanYMO <yanmartinsd3@gmail.com>
2026-06-10 00:16:49 -0300

    feat: Adicionando a nova rota da UsersPage no routes. Adicionando também a rota de Login.

--- src/utils/routes/index.jsx
+++ src/utils/routes/index.jsx
@@ -5,12 +5,15 @@ import {
 } from "react-router";
 import { App } from "../../App";
 import { MonstersPage } from "../../pages/RpgPages/Monsters";
+import { UsersPage } from "../../pages/UsersPage";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" element={<App />} />,
+      <Route path="/login" element={<App />} />,
       <Route path="/monsters" element={<MonstersPage />} />,
+      <Route path="/usersPage" element={<UsersPage />} />,
     </>,
   ),
 );
