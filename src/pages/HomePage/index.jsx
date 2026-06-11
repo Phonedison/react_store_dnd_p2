2074e9cb49e725490a2cfaa9796c8207af275336
joaopedrobr3 <joaopedrobr3@gmail.com>
2026-06-11 18:19:20 -0300

    feat: Criando a base da página home

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,74 @@
+import { useNavigate } from 'react-router'
+import { Div } from '../../components/Div'
+import { Button } from '../../components/Button'
+
+export const Home = () => {
+  const navigate = useNavigate()
+
+  return (
+    <>
+      <title>D&D - Home</title>
+
+      <Div className="container">
+        <Div className="borda-rabisco">
+
+          <Div className="titulo">SEJA BEM VINDO</Div>
+
+          <Div className="em-linha">
+
+            <Div className="cartao" onClick={() => navigate('/monsters')}>
+              <Div className="cartao-interno">
+                <Div className="cartao-frente">
+                  <Div className="titulo">👹</Div>
+                  <Div className="titulo">MONSTROS</Div>
+                </Div>
+              </Div>
+            </Div>
+
+            <Div className="cartao" onClick={() => navigate('/itens')}>
+              <Div className="cartao-interno">
+                <Div className="cartao-frente">
+                  <Div className="titulo">⚔️</Div>
+                  <Div className="titulo">ITENS</Div>
+                </Div>
+              </Div>
+            </Div>
+
+          </Div>
+
+          <Div className="em-linha">
+
+            <Div className="cartao" onClick={() => navigate('/ficha')}>
+              <Div className="cartao-interno">
+                <Div className="cartao-frente">
+                  <Div className="titulo">📜</Div>
+                  <Div className="titulo">CRIAR FICHA</Div>
+                </Div>
+              </Div>
+            </Div>
+
+            <Div className="cartao" onClick={() => navigate('/usersPage')}>
+              <Div className="cartao-interno">
+                <Div className="cartao-frente">
+                  <Div className="titulo">👤</Div>
+                  <Div className="titulo">PERFIL</Div>
+                </Div>
+              </Div>
+            </Div>
+
+          </Div>
+
+          <Div className="container-botoes">
+            <Button
+              className="button confirm"
+              onClick={() => navigate('/monsters')}
+            >
+              Começar Aventura
+            </Button>
+          </Div>
+
+        </Div>
+      </Div>
+    </>
+  )
+}
--- src/utils/routes/index.jsx
+++ src/utils/routes/index.jsx
@@ -6,14 +6,17 @@ import {
 import { App } from "../../App";
 import { MonstersPage } from "../../pages/RpgPages/Monsters";
 import { UsersPage } from "../../pages/UsersPage";
+import { Home } from "../../pages/HomePage";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" element={<App />} />,
       <Route path="/login" element={<App />} />,
+      <Route path="/home" element={<Home/>} />,
       <Route path="/monsters" element={<MonstersPage />} />,
       <Route path="/usersPage" element={<UsersPage />} />,
+
     </>,
   ),
 );
