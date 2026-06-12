58e038df8134e0d61b161a15165af4511ec0d7bc
joaopedrobr3 <joaopedrobr3@gmail.com>
2026-06-12 19:21:44 -0300

    feat: Adicionando um card de explicação do que é DnD

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,7 +1,7 @@
 // import { useNavigate } from 'react-router'
 import { Div } from '../../components/Div'
 // import { Button } from '../../components/Button'
-
+import { Navbar } from '../../components/Header'
 
 export const Home = () => {
   
@@ -11,41 +11,38 @@ export const Home = () => {
       <title>D&D - Home</title>
 
       
-        
+       <Navbar title={"Início"} /> 
       <Div className="home-container" >
          <Div className="cabecalho" >
           
-            
-            
-            
-            <Div className="titulo-home">
-             <h1>BEM VINDO À AVENTURA</h1>
+          <Div className="titulo-home">
+             <h1>BEM VINDO À AVENTURA!</h1>
            </Div>
-
-           
-        </Div>
-
-
-         <Div className="em-linha">      
-          
-           
-
-            
-            
-
-         
-
-            
-            </Div>
-
-            <Div className="em-linha"> 
-            
-            
-            
           </Div>
+       
 
-
+       <Div className= "info-home-card">     
+        <Div className="titulo-home">
+             <h3>O que é Dungeons and Dragons?</h3>
         </Div>
+        <h1>Dungeons & Dragons (D&D) é o RPG de mesa mais famoso do mundo, onde a imaginação é o limite. Neste jogo, os participantes assumem o papel de heróis 
+           únicos e embarcam em aventuras épicas repletas de magia, mistérios, criaturas fantásticas e desafios emocionantes. Guiados por um Mestre de Jogo, 
+           os jogadores exploram mundos incríveis, tomam decisões que influenciam a história e utilizam dados para determinar o resultado de suas ações.
+           Mais do que um simples jogo, D&D incentiva a criatividade, o trabalho em equipe e a construção de narrativas inesquecíveis. 
+           Seja enfrentando dragões, explorando masmorras antigas ou desvendando conspirações em reinos distantes, cada sessão oferece uma experiência única, 
+           moldada pelas escolhas dos próprios jogadores.
+           Descubra um universo de aventuras sem limites e viva histórias que você jamais esquecerá! 
+           </h1>
+       </Div>
+       </Div>
+    
+            
+            
+            
+            
+        
+
+
       
     </>
   )
