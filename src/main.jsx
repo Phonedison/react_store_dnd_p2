2ff94f9046fac004eaccc9ab515fa75df4fd8732
Lucas Leal da Silva <lucasleal00100@gmail.com>
2026-06-12 15:04:45 -0300

    feat: definindo as rotas que receberam o tipo de usuário.

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
+import { UserProvider } from "./contexts/UserContext";
 import "./styles/main.css";
 import { router } from "./utils/routes";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <UserProvider>
+      <RouterProvider router={router} />
+    </UserProvider>
   </StrictMode>,
 );
