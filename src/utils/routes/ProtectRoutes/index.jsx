02c35c9ce569b43561df024678d96f0bfda8f3e7
Lucas Leal da Silva <lucasleal00100@gmail.com>
2026-06-20 13:33:52 -0300

    fix: passagem de parametros de HomePage para Login

--- src/utils/routes/ProtectRoutes/index.jsx
+++ src/utils/routes/ProtectRoutes/index.jsx
@@ -4,10 +4,10 @@ import { useType } from "../../../contexts/index";
 export function ProtectedRoute() {
   const { login } = useType();
   const location = useLocation();
-  const perfilAutenticado = location.state?.typePerfil || login;
+  // const perfilAutenticado = location.state?.typePerfil || login;
 
-  if (!perfilAutenticado) {
-    return <Navigate to="/login" replace />;
+  if (!login) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
   return <Outlet />;
